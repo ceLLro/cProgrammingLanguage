2652ee816e3d718a83dca541404dfd5e68ce4072
Adrian <cellro18@gmail.com>
2019-09-16 10:45:41 +0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -14,17 +14,16 @@ int sizeOfArray = sizeof (arr) / sizeof (int);
 
 int main (){
        
-        for(int i = 0; i < 20; i++){
-        printf("%d  ", arr[i]);}
-    
-    
+    for(int i = 0; i < 20; i++){
+    printf("%d  ", arr[i]);}
+  
     int last = 0;
     for (int i = 0; i < sizeOfArray; i++){
         for (int j = 0; j < sizeOfArray - 1; j++){
             if (arr[j] > arr[j + 1]){
                 swap (&arr[j], &arr[j + 1]);
             }
-	    }
+	}
     }
     printf("\n\n");
     for(int i = 0; i < 20; i++){
